36a8a35527ce69a19f470b2dbb36a10e59163df0
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-10-15 16:06:00 -0500

    14 - Django templates: Using a custom template

--- src/pages/views.py
+++ src/pages/views.py
@@ -7,7 +7,8 @@ from django.shortcuts import render
 def home_view(request, *args, **kwargs):
     print(args, kwargs)
     print(request.user)
-    return HttpResponse("<h1>Hello World</h1>")
+    # return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "home.html", {})
 
 
 def contact_view(*args, **kwargs):
