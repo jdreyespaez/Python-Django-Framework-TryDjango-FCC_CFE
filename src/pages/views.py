1ededfea2ce45df0f95fc1dd406eb673ca4e5f8b
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-10-15 15:58:07 -0500

    14 - Django templates: Logging username through request.user

--- src/pages/views.py
+++ src/pages/views.py
@@ -4,7 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def home_view(*args, **kwargs):
+def home_view(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
     return HttpResponse("<h1>Hello World</h1>")
 
 
