6008ef766acbf94a7a4122a9f8b2e59385160e1a
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-10-16 07:29:43 -0500

    14 - Django templates: adding a render HTML page for Contact Page

--- src/pages/views.py
+++ src/pages/views.py
@@ -11,5 +11,5 @@ def home_view(request, *args, **kwargs):
     return render(request, "home.html", {})
 
 
-def contact_view(*args, **kwargs):
-    return HttpResponse("<h1>Contact Page</h1>")
+def contact_view(request, *args, **kwargs):
+    return render(request, "contact.html", {})
